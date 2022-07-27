eb62dd7d4bec55780d5dc8d3e8d8e6988a16b3c7
Jose-Tejero <j_jtejero@hotmail.com>
2022-07-26 21:19:14 -0500

    Servicios creados

--- src/todos/todos.http.js
+++ src/todos/todos.http.js
@@ -13,3 +13,64 @@ const getAll = (req, res) => {
     response: data,
   });
 };
+
+const getById = (req, res) => {
+  const id = Number(req.params.id);
+  if (id) {
+    const data = getTodoById(id);
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res.status(400).json({ message: "Invalid Id" });
+    }
+  } else {
+    res.status(400).json({ message: "Id is not a number" });
+  }
+};
+
+const postNewTodo = (req, res) => {
+  const data = req.body;
+  if (data.title && data.description && data.status) {
+    const newTodo = createTodo(data);
+    res.status(201).json({ message: "Todo created", data: newTodo });
+  } else {
+    res.status(400).json({ message: "Invalid arguments" });
+  }
+};
+
+const putTodo = (req, res) => {
+  const id = Number(req.params.id);
+  const data = req.body;
+  const newTodoEdited = editTodo(id, data);
+  if (data.title && data.description && data.status) {
+    if (newTodoEdited) {
+      res.status(200).json({ message: "Todo edited" });
+    } else {
+      res.status(400).json({ message: "Invalid Id, todo doesn't exist" });
+    }
+  } else {
+    res.status(400).json({ message: "Invalid arguments" });
+  }
+};
+
+const delTodo = (req, res) => {
+  const id = Number(req.params.id);
+  if (id) {
+    const data = deleteTodo(id);
+    if (data) {
+      res.status(200).json({ message: "Todo deleted" });
+    } else {
+      res.status(400).json({ message: "Invalid Id, todo doesn't exist" });
+    }
+  } else {
+    res.status(400).json({ message: "Id is not a number" });
+  }
+};
+
+module.exports = {
+  getAll,
+  getById,
+  postNewTodo,
+  putTodo,
+  delTodo,
+};
